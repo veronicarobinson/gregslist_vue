03b14f29109ae02e5f2f61bb7a69a053712adf63
Jeremy Fowler <jembyfowler@gmail.com>
2025-09-17 09:49:37 -0600

    getting cars

--- src/pages/CarsPage.vue
+++ src/pages/CarsPage.vue
@@ -1,5 +1,22 @@
 <script setup>
+import { carsService } from '@/services/CarsService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted } from 'vue';
 
+onMounted(() => {
+  getCars()
+})
+
+
+async function getCars() {
+  try {
+    await carsService.getCars()
+  } catch (error) {
+    Pop.error(error)
+    logger.error('COULD NOT GET CARS', error)
+  }
+}
 </script>
 
 
